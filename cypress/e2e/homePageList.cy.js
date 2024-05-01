a7a29fec1592163f59fc362c7392c6149839495a
elgaricito <edgarjaguirre@hotmail.com>
2024-05-01 17:06:24 -0300

    Created the fist test, that compare the links in the page with a saved fixture

---
+++ cypress/e2e/homePageList.cy.js
@@ -0,0 +1,17 @@
+import {homePage} from "../pageObject/HomePage";
+describe('Home page all test', () => {
+    it('Test each link with a previous saved fixture', () => {
+        cy.visit("/");
+        cy.fixture('links').then(links=>{
+            let index=0;
+            for (let key in links) {
+                let link = links[key];
+                homePage.getElementOfList().eq(index).then(element=>{
+                    //console.log(element.text());
+                    assert.equal(element.text(),link);
+                   });
+                   index++;
+            }
+        })
+    });
+});
